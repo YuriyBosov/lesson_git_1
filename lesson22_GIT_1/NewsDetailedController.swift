55f4b01d35675adece37059b5408c5678c540852
Yurii Bosov <bosov.yu@gmail.com>
2020-05-08 20:40:51 +0300

    Added news detailed controller

---
+++ lesson22_GIT_1/NewsDetailedController.swift
@@ -0,0 +1,30 @@
+//
+//  NewsDetailedController.swift
+//  lesson22_GIT_1
+//
+//  Created by Yurii Bosov on 08.05.2020.
+//  Copyright © 2020 Bosov. All rights reserved.
+//
+
+import UIKit
+
+class NewsDetailedController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
