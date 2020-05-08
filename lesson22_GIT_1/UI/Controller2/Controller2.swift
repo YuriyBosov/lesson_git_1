a69325868734c573f90f641fdaff4b35d0074d58
Roman Berezin <romanberezinua@gmail.com>
2020-05-08 20:41:44 +0300

    Created Controller2

---
+++ lesson22_GIT_1/UI/Controller2/Controller2.swift
@@ -0,0 +1,30 @@
+//
+//  Controller2.swift
+//  lesson22_GIT_1
+//
+//  Created by Roman Berezin on 08.05.2020.
+//  Copyright © 2020 Bosov. All rights reserved.
+//
+
+import UIKit
+
+class Controller2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
