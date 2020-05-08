66072fef63b94f1ec18d88a8b0cdf35a32ce0a1d
Yurii Bosov <bosov.yu@gmail.com>
2020-05-08 19:15:06 +0300

    add project's files

---
+++ lesson22_GIT_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson22_GIT_1
+//
+//  Created by Yurii Bosov on 08.05.2020.
+//  Copyright © 2020 Bosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
